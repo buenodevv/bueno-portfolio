55fb1d375f8cffd3924f17f6e21f015cafdc4586
Rodrigo Bueno <buenodevv@gmail.com>
2024-06-12 09:26:44 -0300

    feat: image animation added

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span>Software Developer</span>
             <h1 className="text-[48px] xl:text-[80px] font-semibold leading-[1.1] mb-6">
               Hello I'm <br /> <span className="text-rb">Rodrigo Bueno</span>
@@ -34,7 +34,9 @@ export default function Home() {
             </div>
           </div>
           {/* photo */}
-          <div><Photo /></div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
