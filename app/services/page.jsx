fae6adfaac24a82e65feaac639968f66065874f1
Rodrigo Bueno <buenodevv@gmail.com>
2024-06-14 06:27:16 -0300

    feat: component stats complete

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,58 @@
+"use client"
+import Link from 'next/link'
+import { BsArrowDownRight } from 'react-icons/bs'
+
+const services = [
+  {
+    num: '01',
+    title: "Web Development",
+    description: "We develop web applications using the latest technologies.",
+    href: "/services/web-development",
+  },
+  {
+    num: '02',
+    title: "UI/UX Design",
+    description: "We create intuitive and engaging user interfaces with a focus on user experience design.",
+    href: "/services/web-design",
+  },
+  {
+    num: '03',
+    title: "Logo Design",
+    description: "We create visually appealing logos that reflect your brand identity.",
+    href: "/services/web-design",
+  },
+  {
+    num: '04',
+    title: "SEO",
+    description: "We optimize your website for search engines to increase your visibility.",
+    href: "/services/web-design",
+  },
+];
+
+import { motion } from 'framer-motion'
+
 function Services() {
   return (
-    <div>Page Sevices</div>
+    <section className=' min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+      <div className="container mx-auto">
+        <motion.div
+          initial={{opacity: 0}}
+          animate={{opacity: 1, 
+          transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}  
+          }}
+          
+        >
+          {services.map((service, index)=>{
+            return <div key={index}>
+              <div>
+                <div>{service.num}</div>
+              </div>
+            </div>
+          })}
+        </motion.div>
+      </div>
+      
+    </section>
   )
 }
 
