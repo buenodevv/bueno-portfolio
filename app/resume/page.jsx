dedfc985be9731562cdb73b4973abcab4af1257e
Rodrigo Bueno <buenodevv@gmail.com>
2024-06-17 06:29:18 -0300

    feat: resume complete

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,129 @@
+'use client'
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaNodeJs,
+  FaReact,
+  FaFigma,
+  FaGithub,
+} from "react-icons/fa"
+
+import {SiTailwindcss, SiNextdotjs} from "react-icons/si"
+
+//about data
+const about = {
+  title: "About Me",
+  description: "I'm a full stack developer with a passion for creating user-friendly and efficient web applications.",
+  Info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Rodrigo Bueno",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+55) 15 99701 1809",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "6+ Years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "rodrigojbueno",
+    },
+    {
+      fieldName: "Nacionality",
+      fieldValue: "Brazilian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "buenoevv@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Portuguese",
+    },
+  ]
+};
+const experience = {
+  icon: 'assets/resume/badge.svg',
+  title: "My experience",
+  description: "I'm a full stack developer with a passion for creating user-friendly and efficient web applications.",
+  itens: [
+    {
+      company: "Tech Solutions Inc ",
+      position: "Full stack developer ",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Web Design Studio ",
+      position: "Front-end developer Intern",
+      duration: "Summer 2021",
+    },
+    {
+      company: "E-commerce Startup",
+      position: "Freelance Web Developer",
+      duration: "2020 - 2021",
+    },
+    {
+      company: "Tech Academy",
+      position: "Freelance Web Developer",
+      duration: "2019 - 2020",
+    },
+    {
+      company: "Digital Agency",
+      position: "UI/UX Designer",
+      duration: "2018 - 2019",
+    },
+    {
+      company: "Software Development Company",
+      position: "Junior Developer",
+      duration: "2017 - 2018",
+    }
+  ]
+}
+const education = {
+  icon: 'assets/resume/cap.svg',
+  title: "My education",
+  description: "I'm a full stack developer with a passion for creating user-friendly and efficient web applications.",
+  itens: [
+    {
+      institution: "Online Course Plataform",
+      course: "Full Stack Web Development Bootcamp",
+      duration: "2023",
+    },
+    {
+      institution: "CodeAcademy",
+      course: "Front-end Tracke",
+      duration: "2023",
+    },
+    {
+      institution: "Online Course",
+      course: "Programming Course",
+      duration: "2020 - 2021",
+    },
+    {
+      institution: "TEch Institute",
+      course: "Certifield Web Developer",
+      duration: "2019",
+    },
+    {
+      institution: "Design School",
+      course: "Diploma in Graphic Design",
+      duration: "2016 - 2018",
+    },
+    {
+      institution: "Community College",
+      course: "Associate Degree in Computer Science",
+      duration: "2014 - 2016",
+    },
+  ]
+}
 function Services() {
   return (
     <div>Page resume</div>
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -36,22 +36,41 @@ function Services() {
     <section className=' min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className="container mx-auto">
         <motion.div
-          initial={{opacity: 0}}
-          animate={{opacity: 1, 
-          transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}  
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' }
           }}
-          
+          className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'
         >
-          {services.map((service, index)=>{
-            return <div key={index}>
-              <div>
-                <div>{service.num}</div>
+          {services.map((service, index) => {
+            return <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
+              {/*top*/}
+              <div className='w-full flex justify-between items-center'>
+                <div className='text-5xl font-extrabold text-outline 
+                text-transparent group-hover:text-outline-hover transition-all
+                duration-500
+                '>{service.num}</div>
+                <Link href={service.href}
+                className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-rb
+                transition-all duration-500 flex items-center justify-center hover:-rotate-45'
+                >
+                  <BsArrowDownRight className='text-primary text-3xl' />
+                </Link>
               </div>
+              {/*title*/}
+              <h2 className='text-[42px] font-bold leading-[50px]
+              text-white group-hover:text-rb transition-all duration-500
+              '>{service.title}</h2>
+              {/*description*/}
+              <p className='text-white/60'>{service.description}</p>
+              {/*border*/}
+              <div className='border-b border-white/20 w-full'></div>
             </div>
           })}
         </motion.div>
       </div>
-      
+
     </section>
   )
 }
