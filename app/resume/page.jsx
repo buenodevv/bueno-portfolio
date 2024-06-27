afe5480e8aebc95bd96277d833ce401b841b0383
Rodrigo Bueno <buenodevv@gmail.com>
2024-06-27 06:16:39 -0300

    feat: resume text center altered

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -301,7 +301,7 @@ function Services() {
             >
               <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">{about.description}</p>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
                 <ul className="grid grid-col-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
                   {about.Info.map((item, index) => {
                     return (
